662102a58bb880efacc1f2d2d033259b6c51ff89
Uchio KONDO <udzura@pepabo.com>
2017-02-01 20:43:08 +0900

    Initial

---
+++ lib/rack/mixed/contents.rb
@@ -0,0 +1,9 @@
+require "rack/mixed/contents/version"
+
+module Rack
+  module Mixed
+    module Contents
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/mixed/contents/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Mixed
+    module Contents
+      VERSION = "0.1.0"
+    end
+  end
+end
